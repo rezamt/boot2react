33e676f5195b497d5ab96349fe61a27a0aa3f4b9
Reza Motevallizadeh <rezamt@gmail.com>
2017-01-15 23:24:12 +1100

    Authorization has been added.

--- src/main/java/io/inuit/EmployeeRepository.java
+++ src/main/java/io/inuit/EmployeeRepository.java
@@ -1,6 +1,13 @@
 package io.inuit;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+    //PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    // I have simply used the href of stormpath here.
+    @PreAuthorize("hasAuthority('https://api.stormpath.com/v1/groups/2tsC4r8UYv0ndZrX3xcslg')")
+    @Override
+    void delete(Long aLong);
 }
